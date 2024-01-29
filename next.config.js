f4fad3fc001c72e7879c5b578c39c4bf323c4f8d
Braydon Jones <bradofrado@gmail.com>
2024-01-29 10:30:14 -0700

    Fix error with plugin path

--- next.config.js
+++ next.config.js
@@ -2,7 +2,12 @@
 const nextConfig = {
     experimental: {
         swcPlugins: [
-            [require.resolve("./packages/swc-plugin/target/wasm32-wasi/release/harmony_plugin.wasm"), {rootDir: __dirname}],
+            [
+                process.env.NODE_ENV === 'development' ? 
+                    require.resolve("./packages/swc-plugin/target/wasm32-wasi/release/harmony_plugin.wasm") : 
+                    require.resolve('./packages/swc-plugin/harmony_plugin.wasm'), 
+                    {rootDir: __dirname}
+            ],
         ],
     },
 }
