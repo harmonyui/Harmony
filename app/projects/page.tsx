998653937220bef62549bd3358df2dd9ff4758d2
SzuJanLin <szujan.lin@gmail.com>
2024-04-12 10:13:45 -0400

    finish onboarding optimization

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,20 +1,25 @@
 import { SideNav } from "../../utils/side-nav";
-import {  ModalProvider } from "@harmony/ui/src/components/core/modal";
+import { ModalProvider } from "@harmony/ui/src/components/core/modal";
 import React, { useRef } from "react";
 import { withAuth } from "../../utils/protected-routes-hoc";
 import { getBranches } from "../../src/server/api/routers/branch";
 import { ProjectDisplay } from "@harmony/ui/src/components/features/project";
 
+import { redirect } from "next/navigation";
+import { ProjectSetUp } from "./setup";
 
-const ProjectsPage = withAuth(async ({ctx}) => {
-	const branches = ctx.session.account.repository ? await getBranches({prisma: ctx.prisma, repositoryId: ctx.session.account.repository.id}) : undefined;
+
+const ProjectsPage = withAuth(async ({ ctx }) => {
+	const branches = ctx.session.account.repository ? await getBranches({ prisma: ctx.prisma, repositoryId: ctx.session.account.repository.id }) : undefined;
+	const onClick = (): void => {
+		redirect('/setup');
+	}
 
 	return (
 		<ModalProvider>
 			<SideNav>
-				{branches ? <ProjectDisplay Projectes={branches} defaultUrl={ctx.session.account.repository!.defaultUrl}/> : <div>
-					No Repositories
-				</div>}
+				{branches ? <ProjectDisplay Projectes={branches} defaultUrl={ctx.session.account.repository!.defaultUrl} /> : <ProjectSetUp />
+				}
 				{/* <SnappingDemo/> */}
 			</SideNav>
 		</ModalProvider>
