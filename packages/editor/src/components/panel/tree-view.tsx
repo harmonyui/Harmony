8972ac0de2795efd920aa41558974803b13c5145
TannerHelms <tannerhelms01@gmail.com>
2024-05-21 12:04:16 -0600

    Issue #94 - Didn't stage all the files so here they are

--- packages/editor/src/components/panel/tree-view.tsx
+++ packages/editor/src/components/panel/tree-view.tsx
@@ -1,11 +1,26 @@
 import { getClass } from "@harmony/util/src/utils/common";
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import { set } from "zod";
+import {
+	ContextMenuComponent,
+	TreeViewComponent,
+  } from "@syncfusion/ej2-react-navigations";
+  import { enableRipple } from "@syncfusion/ej2-base";
+  enableRipple(true);
+  let menuObj;
+  let treeObj;
 export interface TreeViewItem<T = string> {
 	id: T;
 	content: React.ReactNode,
 	items: TreeViewItem<T>[],
 	selected: boolean,
+}
+
+export interface TransformNode<T = string> {
+	id: T,
+	type: React.ReactNode,
+	subChild: TransformNode<T>[],
+	expanded: boolean,
 }
 const isSelected = <T,>(item: TreeViewItem<T>): boolean => {
 	if (item.selected) return true;
@@ -14,6 +29,7 @@ const isSelected = <T,>(item: TreeViewItem<T>): boolean => {
 
 	return false;
 }
+
 
 export const TreeViewItem = <T,>({item, onClick, onHover}: {item: TreeViewItem<T>, onClick: (item: TreeViewItem<T>) => void, onHover: (item: TreeViewItem<T>) => void,}) => {
 	const [expand, setExpand] = useState(isSelected(item));
@@ -56,11 +72,47 @@ export const TreeViewItem = <T,>({item, onClick, onHover}: {item: TreeViewItem<T
 }
 
 export const TreeView = <T,>({items, expand, onClick, onHover}: {items: TreeViewItem<T>[], expand?: boolean, onClick: (item: TreeViewItem<T>) => void, onHover: (item: TreeViewItem<T>) => void}) => {
-	return <>
-		<ul className={getClass('!hw-visible hw-ml-2', expand ? '' : 'hw-hidden')}>
-			{items.map(item => <>
-				<TreeViewItem item={item} onClick={onClick} onHover={onHover}/>
-			</>)}
-		</ul>
-	</>
+	const  [transformedItems, setTransformedItems] = useState<TransformNode<T>[]>(); 
+	
+	function transform(node: TreeViewItem<any>): TransformNode<T> {
+		const {id} = node.id;
+		
+		const transformedNode: TransformNode<T> = {
+			id,
+			type: String(node.content), // Ensuring content is cast to string
+			expanded: false,
+			subChild: [] as TransformNode<T>[],
+		};
+	
+		if (node.items && node.items.length > 0) {
+			transformedNode.subChild = node.items.map(transform);
+		}
+	
+		return transformedNode;
+	}
+
+	useEffect(() => {
+		let i = items.map((item) => {
+			return transform(item)
+		})
+        i[0].expanded = true
+		setTransformedItems(i);
+	}, [items])
+
+	useEffect(() => {
+		console.log(transformedItems)
+	}, [transformedItems])
+
+
+	function nodeclicked(args: any) {
+		console.log(args.node.data)
+    }
+    const fields: Object = { dataSource: transformedItems, id: 'id', text: 'type', child: 'subChild' };
+
+    if (!transformedItems) return <p>loading....</p>;
+
+    return (
+        // specifies the tag for render the TreeView component
+        <TreeViewComponent fields={fields} allowDragAndDrop={true} nodeClicked={nodeclicked.bind(this)} />
+    );
 }
