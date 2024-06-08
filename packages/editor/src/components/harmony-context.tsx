0ecd44f5a3ac3da7d64918fc1f358b514b66f06e
TannerHelms <tannerhelms01@gmail.com>
2024-06-08 15:41:24 -0600

    134 Component Mocks and Linking/Unlinking Logic (#152)
    * Merge branch 'staging' of https://github.com/bradofrado/Harmony into 98-layering-reorder-error-elements
    
    # Conflicts:
    #	packages/editor/src/components/panel/toolbar-panel.tsx
    #	packages/editor/src/components/panel/tree-view.tsx
    #	packages/server/src/api/routers/editor.ts
    
    * Moved console.log below execute Command

--- packages/editor/src/components/harmony-context.tsx
+++ packages/editor/src/components/harmony-context.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable @typescript-eslint/require-await -- ok*/
+import { ComponentElement } from "@harmony/db/lib/generated/client";
 import type { Font } from "@harmony/util/src/fonts";
 import type { PullRequest } from "@harmony/util/src/types/branch";
-import type { BehaviorType, ComponentUpdate } from "@harmony/util/src/types/component";
+import type { BehaviorType, ComponentUpdate, HarmonyComponentInfo } from "@harmony/util/src/types/component";
 import type { PublishRequest, PublishResponse } from "@harmony/util/src/types/network";
 import type { Environment } from "@harmony/util/src/utils/component";
 import { createContext, useContext } from "react";
@@ -29,7 +30,7 @@ interface HarmonyContextProps {
 	fonts?: Font[];
 	onFlexToggle: () => void;
 	scale: number;
-	onScaleChange: (scale: number, cursorPos: {x: number, y: number}) => void;
+	onScaleChange: (scale: number, cursorPos: { x: number, y: number }) => void;
 	onClose: () => void;
 	error: string | undefined;
 	setError: (value: string | undefined) => void;
@@ -39,17 +40,18 @@ interface HarmonyContextProps {
 	showGiveFeedback: boolean;
 	setShowGiveFeedback: (value: boolean) => void;
 	isDemo: boolean;
-	currentBranch: {id: string, name: string} | undefined;
+	currentBranch: { id: string, name: string } | undefined;
 	behaviors: BehaviorType[];
 	setBehaviors: (value: BehaviorType[]) => void;
 	isGlobal: boolean;
 	setIsGlobal: (value: boolean) => void;
 	onComponentSelect: (component: HTMLElement) => void;
 	onComponentHover: (component: HTMLElement) => void;
-    selectedComponent: HTMLElement | undefined;
+	selectedComponent: HTMLElement | undefined;
 	onAttributesChange: (updates: ComponentUpdateWithoutGlobal[], execute?: boolean) => void;
+	harmonyComponents: HarmonyComponentInfo[];
 }
-export const HarmonyContext = createContext<HarmonyContextProps>({branchId: '', pullRequest: undefined, publish: asyncnoop, isSaving: false, setIsSaving: noop, setPullRequest: noop, displayMode: 'designer', changeMode: noop, publishState: undefined, setPublishState: noop, onFlexToggle: noop, scale: 1, onScaleChange: noop, onClose: noop, error: undefined, setError: noop, environment: 'production', showWelcomeScreen: false, setShowWelcomeScreen: noop, showGiveFeedback: false, setShowGiveFeedback: noop, isDemo: false, currentBranch: undefined, behaviors: [], setBehaviors: noop, isGlobal: false, setIsGlobal: noop, onComponentHover: noop, onComponentSelect: noop, selectedComponent: undefined, onAttributesChange: noop});
+export const HarmonyContext = createContext<HarmonyContextProps>({ branchId: '', pullRequest: undefined, publish: asyncnoop, isSaving: false, setIsSaving: noop, setPullRequest: noop, displayMode: 'designer', changeMode: noop, publishState: undefined, setPublishState: noop, onFlexToggle: noop, scale: 1, onScaleChange: noop, onClose: noop, error: undefined, setError: noop, environment: 'production', showWelcomeScreen: false, setShowWelcomeScreen: noop, showGiveFeedback: false, setShowGiveFeedback: noop, isDemo: false, currentBranch: undefined, behaviors: [], setBehaviors: noop, isGlobal: false, setIsGlobal: noop, onComponentHover: noop, onComponentSelect: noop, selectedComponent: undefined, onAttributesChange: noop, harmonyComponents: [] });
 
 export const useHarmonyContext = () => {
 	const context = useContext(HarmonyContext);
