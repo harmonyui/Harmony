339523eeaf582da4a43bec33d75301cec6b10c0d
Braydon Jones <bradofrado@gmail.com>
2024-06-10 17:30:39 -0600

    Fix bug with white space tailwind merge problem

--- packages/server/src/api/services/publish/code-updator.spec.ts
+++ packages/server/src/api/services/publish/code-updator.spec.ts
@@ -1,10 +1,12 @@
 /* eslint-disable @typescript-eslint/dot-notation -- ok*/
 /* eslint-disable @typescript-eslint/require-await -- ok*/
 import { describe, expect, it } from "vitest";
-import { CodeUpdator, UpdateInfo } from "./code-updator";
-import { GitRepository } from "../../repository/git/types";
-import { getCodeInfoFromFile, HarmonyComponentWithNode, indexFiles } from "../indexor/indexor";
-import { Repository } from "@harmony/util/src/types/branch";
+import type { Repository } from "@harmony/util/src/types/branch";
+import type { GitRepository } from "../../repository/git/types";
+import type { HarmonyComponentWithNode } from "../indexor/indexor";
+import { getCodeInfoFromFile } from "../indexor/indexor";
+import type { UpdateInfo } from "./code-updator";
+import { CodeUpdator } from "./code-updator";
 
 describe("code-updator", () => {
     const expectLocationOfString = (file: TestFile, actualLocation: {start: number, end: number}, expectedString: string): void => {
@@ -98,6 +100,28 @@ describe("code-updator", () => {
             expect(changes[1].updatedCode).toBe('hw-flex hw-flex-row hw-pl-2.5')
             expectLocationOfString(file, changes[1].location, 'hw-flex hw-flex-col');
         })
+
+        it("Should handle non tailwind classes", async () => {
+            const file: TestFile = 'nonTailwindClass';
+            const {codeUpdator, elementInstances} = setupGitRepo(file);
+            const updateInfos: UpdateInfo[] = [
+                {
+                    component: elementInstances[0],
+                    attributes: elementInstances[0].props,
+                    value: 'margin-left:3px;',
+                    oldValue: '',
+                    type: 'className',
+                    name: '',
+                    componentId: elementInstances[0].id
+                },
+            ]
+
+            const changes = (await Promise.all(updateInfos.map(updateInfo => codeUpdator['getChangeAndLocation'](updateInfo, 'master')))).flat();
+
+            expect(changes.length).toBe(1);
+            expect(changes[0].updatedCode).toBe('flex cursor-default flex-col items-start justify-between space-y-2 rounded-lg border border-slate-200 bg-white p-4 text-left shadow-sm ml-[3px]')
+            expectLocationOfString(file, changes[0].location, 'flex cursor-default flex-col items-start justify-between space-y-2 rounded-lg border border-slate-200 bg-white p-4 text-left shadow-sm');
+        })
     })
 })
 
@@ -108,6 +132,15 @@ const testFiles = {
             return (
                 <div className="hw-group hw-flex hw-py-2">
                     <h1 className="hw-flex hw-flex-col">Hello there</h1>
+                </div>
+            )
+        }
+    `,
+    'nonTailwindClass': `
+        const NonTailwindClass = () => {
+            return (
+                <div className="flex cursor-default flex-col items-start justify-between space-y-2 rounded-lg border border-slate-200 bg-white p-4 text-left shadow-sm">
+                    Bob
                 </div>
             )
         }
