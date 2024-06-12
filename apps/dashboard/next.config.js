d267cb6a8ece1a294619f33852ca42826f42fc1a
Braydon Jones <92066133+bradofrado@users.noreply.github.com>
2024-06-12 11:26:06 -0600

    Handle spread attributes (#165)
    * Handle spread attributes for class name and children
    
    * Be able to handle multiple layers of spread
    
    * Add the prisma workaround back in

--- apps/dashboard/next.config.js
+++ apps/dashboard/next.config.js
@@ -1,7 +1,14 @@
 const path = require('node:path');
+const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+        config.plugins = [...config.plugins, new PrismaPlugin()];
+        }
+        return config;
+    },
     experimental: {
         swcPlugins: process.env.ENV !== 'production' ? [
             [
