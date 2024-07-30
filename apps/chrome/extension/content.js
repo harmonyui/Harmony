1205a7ca88345d35f5e9c2b3884a4ea7f3083f24
Braydon Jones <bradofrado@gmail.com>
2024-07-29 18:26:50 -0600

    Make event listener on chrome extension click

--- apps/chrome/extension/content.js
+++ apps/chrome/extension/content.js
@@ -1,4 +1,4 @@
-/*global document chrome -- ok*/
+/*global document chrome window -- ok*/
 const harmonyEntryPoint = document.createElement('div')
 const harmonyScript = document.createElement('script')
 
@@ -8,3 +8,10 @@ harmonyScript.src = chrome.runtime.getURL('dist/bundle.js')
 harmonyEntryPoint.appendChild(harmonyScript)
 
 document.querySelector('body').appendChild(harmonyEntryPoint)
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === 'extensionIconClicked') {
+    window.dispatchEvent(new CustomEvent('toggleEditor'))
+    sendResponse({ status: 'action performed' })
+  }
+})
