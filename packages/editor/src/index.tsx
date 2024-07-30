ff90acc9a9a4c17421098c22b9e771b6c8d1486d
Braydon Jones <bradofrado@gmail.com>
2024-07-29 20:52:09 -0600

    Can turn on and off harmony through chrome extension

--- packages/editor/src/index.tsx
+++ packages/editor/src/index.tsx
@@ -4,7 +4,8 @@ import { useEffect, useRef } from 'react'
 import ReactDOM from 'react-dom'
 import type { HarmonyProviderProps } from './components/harmony-provider'
 import { HarmonyProvider } from './components/harmony-provider'
-import { HarmonySetup } from './components/harmony-setup'
+import { HarmonySetup, useHarmonySetup } from './components/harmony-setup'
+import { QueryStateProvider } from './components/hooks/query-state'
 
 type HarmonyProvider = (
   options: Omit<HarmonyProviderProps, 'children'>,
@@ -29,7 +30,7 @@ export function HarmonyProviderFunc(
     useEffect(() => {
       if (ref.current) {
         options.setup.setContainer(ref.current)
-        options.setup.changeMode('designer')
+        //options.setup.changeMode(options.mode || 'designer')
       }
     }, [ref])
     return (
@@ -48,12 +49,14 @@ export function HarmonyProviderFunc(
     />
   ) //document.createElement('body');
   ReactDOM.render(
-    <HarmonyProvider {...options}>{container}</HarmonyProvider>,
+    <QueryStateProvider>
+      <HarmonyProvider {...options}>{container}</HarmonyProvider>
+    </QueryStateProvider>,
     harmonyContainer,
   )
 }
 
-export { HarmonySetup }
+export { HarmonySetup, useHarmonySetup }
 
 if (typeof window !== 'undefined') {
   window.HarmonyProvider = HarmonyProviderFunc
