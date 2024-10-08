98e5fb648b779bdf5bf70357c526d3281ccb7e73
Braydon Jones <bradofrado@gmail.com>
2024-10-07 20:07:09 -0600

    one last dark mode fix

--- apps/dashboard/app/(application)/projects/components/setup.tsx
+++ apps/dashboard/app/(application)/projects/components/setup.tsx
@@ -10,7 +10,10 @@ export const ProjectSetUp: React.FunctionComponent = () => {
   }
   return (
     <>
-      <div> No Repositories. Please finish setting up here:</div>
+      <div className='dark:hw-text-white'>
+        {' '}
+        No Repositories. Please finish setting up here:
+      </div>
 
       <Button className='hw-w-fit hw-ml-auto hw-mt-2' onClick={onClick}>
         Continue
