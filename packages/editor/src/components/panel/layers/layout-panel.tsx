026c42e2ae557a3f6fb3557f4f1bae712d4b754f
Braydon Jones <bradofrado@gmail.com>
2024-11-20 17:36:03 -0700

    Selecting an element on the dom shows up on the layout panel

--- packages/editor/src/components/panel/layers/layout-panel.tsx
+++ packages/editor/src/components/panel/layers/layout-panel.tsx
@@ -66,7 +66,7 @@ export const useComponentTreeItems = (
         }
       })
     },
-    [scale],
+    [scale, selectedComponent],
   )
   const treeItems: TreeData<HTMLElement>[] = useMemo(
     () => (root ? (getTreeItems([root]) ?? []) : []),
