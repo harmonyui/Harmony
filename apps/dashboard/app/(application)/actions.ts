c4010800d84ade77833eba42214cf5fa6717ac59
Braydon Jones <bradofrado@gmail.com>
2024-12-04 20:51:53 -0700

    Add cache to get url

--- apps/dashboard/app/(application)/actions.ts
+++ apps/dashboard/app/(application)/actions.ts
@@ -1,17 +1,25 @@
 'use server'
 
-export const getThumbnail = async (url: string): Promise<string> => {
-  const response = await fetch(
-    `https://api.microlink.io/?url=${encodeURIComponent(url)}&screenshot=true`,
-  )
-  if (!response.ok) {
-    throw new Error('There was an error fetching the thumbnail')
-  }
+import { unstable_cache as unstableCache } from 'next/cache'
 
-  const json = (await response.json()) as {
-    data: { screenshot: { url: string } }
-  }
-  const thumbnailImage = json.data.screenshot.url
+export const getThumbnail = unstableCache(
+  async (url: string): Promise<string> => {
+    const response = await fetch(
+      `https://api.microlink.io/?url=${encodeURIComponent(url)}&screenshot=true`,
+    )
+    if (!response.ok) {
+      throw new Error('There was an error fetching the thumbnail')
+    }
 
-  return thumbnailImage
-}
+    const json = (await response.json()) as {
+      data: { screenshot: { url: string } }
+    }
+    const thumbnailImage = json.data.screenshot.url
+
+    return thumbnailImage
+  },
+  undefined,
+  {
+    revalidate: 3600,
+  },
+)
