44c5e037be6e2f0b8d244b85e118130e173eafef
braydonjones <braydon@joinleland.com>
2025-07-27 14:05:35 -0600

    improvement: do the tailwindcss mangle for deployed editor

--- packages/editor/webpack.config.editor.js
+++ packages/editor/webpack.config.editor.js
@@ -1,73 +1,40 @@
-const path = require('node:path');
-const webpack = require('webpack');
+const path = require('node:path')
+const webpack = require('webpack')
+const utmc = require('./unplugin-tailwindcss-mangle/webpack.cjs')
 
 module.exports = {
-    entry: './src/index.prod.tsx',
-    output: {
-        path: path.resolve(__dirname, 'dist/editor'),
-        filename: 'bundle.js',
-        //library: 'HarmonySetup',
-        libraryTarget: 'umd',
-        // publicPath: '/dist',
-        globalObject: 'this',
-        //libraryTarget: 'umd',
-    },
-    target: 'web',
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js', '.jsx'],
-        // alias: {
-        //     '@harmony': path.resolve(__dirname, '../'),
-        //     'react': path.resolve(path.join(__dirname, '../..'), './node_modules/react'),
-        // }
-    },
-    plugins: [
-        new webpack.EnvironmentPlugin({ ...process.env })
+  entry: './src/index.prod.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist/editor'),
+    filename: 'bundle.js',
+    libraryTarget: 'umd',
+    globalObject: 'this',
+  },
+  target: 'web',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
+  plugins: [new webpack.EnvironmentPlugin({ ...process.env }), utmc()],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.(js|jsx)$/,
-                use: 'babel-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader', 
-                    'css-loader',
-                    'postcss-loader'
-                ],
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
-        ],
-    },
-    // externals: {
-    //     react: {
-    //       commonjs: "react",
-    //       commonjs2: "react",
-    //       amd: "React",
-    //       root: "React"
-    //     },
-    //     "react-dom": {
-    //       commonjs: "react-dom",
-    //       commonjs2: "react-dom",
-    //       amd: "ReactDOM",
-    //       root: "ReactDOM"
-    //     },
-    //     url: 'url'
-    //   },
-    // externals: {
-    //     'react-dom': 'ReactDOM'
-    // }
-    // output: {
-    //     globalObject: 'this'
-    // }
+  },
 }
